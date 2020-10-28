de5804b6024a938fbffb8118b23eb3ef19d430de
E. G. Patrick Bos <egpbos@gmail.com>
2020-10-28 10:42:45 +0100

    Complete update_numbers.sh script

--- update_numbers.sh
+++ update_numbers.sh
@@ -1,2 +1,7 @@
 curl -s https://d1bjgq97if6urz.cloudfront.net/Public/Peilingwijzer/Last/Cijfers_Peilingwijzer.xlsx -o Cijfers_Peilingwijzer.xlsx
 python update_numbers.py
+git add peilingen.pkl
+git commit -m "updated numbers from Peilingwijzer"
+git push
+git push heroku main
+
