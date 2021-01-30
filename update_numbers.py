a27cd37632102ba2d768295145db22c3f1c938eb
Patrick Bos <egpbos@gmail.com>
2021-01-30 16:51:49 +0100

    update update_numbers scripts + remove simulations.pkl

--- update_numbers.py
+++ update_numbers.py
@@ -1,6 +1,10 @@
 import pandas as pd
 from collections import namedtuple
 import pickle
+import statistics
+import math
+import itertools
+from tqdm import tqdm
 
 # polls
 
@@ -30,6 +34,35 @@ sims_df = pd.read_csv("https://d1bjgq97if6urz.cloudfront.net/Public/Peilingwijze
 
 sims = {party: tuple(sims_df[party]) for party in sims_df}
 
-with open('simulations.pkl', 'wb') as fh:
-    pickle.dump(sims, fh)
+# preprocess simulations into combinations table for 1000x faster retrieval in webapp
 
+def sum_coalition(data, *parties):
+    result = sum(data[party] for party in parties)
+    if isinstance(result, int) and result == 0:
+        return pd.Series([], dtype='int')
+    else:
+        return result
+
+def powerset(iterable):
+    "powerset([1,2,3]) --> () (1,) (2,) (3,) (1,2) (1,3) (2,3) (1,2,3)"
+    s = list(iterable)
+    return itertools.chain.from_iterable(itertools.combinations(s, r) for r in range(len(s)+1))
+
+def to_Peiling_from_simulations(simulations):
+    def estimate_Peiling(est, std):
+        interval = math.ceil((2 * std))
+        return Peiling(verwacht=int(est),
+                       laag=int(max(0, est - interval)),
+                       hoog=int(min(est + interval, 150)))
+
+    if len(simulations) == 0:
+        return Peiling(0, 0, 0)
+    est = round(statistics.mean(simulations))
+    return estimate_Peiling(est, statistics.stdev(simulations))
+
+table = {}
+for key in tqdm(powerset(sims.keys()), total=2**len(sims.keys())):
+    table[key] = to_Peiling_from_simulations(sum_coalition(sims_df, *key))
+
+with open('table.pkl', 'wb') as fh:
+    pickle.dump(table, fh)
--- update_numbers.sh
+++ update_numbers.sh
@@ -1,6 +1,6 @@
 curl -s https://d1bjgq97if6urz.cloudfront.net/Public/Peilingwijzer/Last/Cijfers_Peilingwijzer.xlsx -o Cijfers_Peilingwijzer.xlsx
 python update_numbers.py
-git add peilingen.pkl
+git add peilingen.pkl table.pkl
 git commit -m "updated numbers from Peilingwijzer"
 git push
 git push heroku main
